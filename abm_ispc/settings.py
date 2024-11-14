94a64840d7d4422162650566285c39c513b33e5c
Marialer27 <maria.alerodriguez@gmail.com>
2024-11-14 19:04:08 -0300

    Modificamos settings.py

--- abm_ispc/settings.py
+++ abm_ispc/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     "rest_framework",
     "corsheaders",
     'django_rest_passwordreset',
